cb541051f0e169e11ac5a1dcab58ed830c1602cd
eliseydudin <dudinelisey630@gmail.com>
2025-04-03 10:47:58 +0300

    oh wowza it now actually works properly

--- examples/test.rs
+++ examples/test.rs
@@ -1,19 +1,18 @@
-use std::{error::Error, fs::File, io::stdout};
+use std::{error::Error, fs::File};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let out = File::create("test.txt")?;
-    logger::init(out);
-
-    log::info!("this writes to file!!");
-
-    let out = stdout();
-    logger::Logger::replace_logger(out);
+    logger::init_stdout();
+    log::set_max_level(log::LevelFilter::Trace);
 
     log::info!("info");
     log::error!("error");
     log::debug!("debug");
     log::trace!("trace");
     log::warn!("warn");
+
+    let new_file = File::create("test.txt")?;
+    logger::swap(new_file);
+    log::info!("hello kro...");
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,87 +1,55 @@
-#![feature(thread_id_value)]
-#![feature(try_blocks)]
-
 use chrono::Utc;
 use core::fmt;
 use log::{Level, Log};
 use std::{
-    io::{self, BufWriter},
-    sync, thread,
+    io,
+    sync::{LazyLock, Mutex, MutexGuard},
 };
 
-pub struct Logger {
-    inner: Box<sync::Mutex<dyn io::Write + Send>>,
+enum Inner {
+    Stdout,
+    Stderr,
+    Other(Box<dyn io::Write + Send>),
 }
+
+pub struct Logger(Mutex<Inner>);
 
 impl fmt::Debug for Logger {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Logger")
-            .field("inner", &"<locked>")
-            .finish()
+        let inner = match &*self.inner() {
+            Inner::Stdout => "stdout",
+            Inner::Stderr => "stderr",
+            Inner::Other(_) => "<locked>",
+        };
+
+        f.debug_tuple("Logger").field(&inner).finish()
     }
 }
-
-unsafe impl Send for Logger {}
 
 impl Logger {
-    pub fn new<F>(s: F) -> Self
+    pub fn new<B>(buffer: B) -> Self
     where
-        F: io::Write + Send + 'static,
+        B: io::Write + Send + 'static,
     {
-        Self {
-            inner: Box::new(sync::Mutex::new(s)),
+        Self(Mutex::new(Inner::Other(Box::new(buffer))))
+    }
+
+    pub fn stdout() -> Self {
+        Self(Mutex::new(Inner::Stdout))
+    }
+
+    pub fn stderr() -> Self {
+        Self(Mutex::new(Inner::Stderr))
+    }
+
+    fn inner(&self) -> MutexGuard<'_, Inner> {
+        match self.0.lock() {
+            Ok(lock) => lock,
+            Err(_) => {
+                self.0.clear_poison();
+                self.inner()
+            }
         }
-    }
-
-    pub fn new_buffered<F>(file: F) -> Self
-    where
-        F: io::Write + Send + 'static,
-    {
-        let bufwriter = BufWriter::with_capacity(50, file);
-        Self::new(bufwriter)
-    }
-
-    pub fn replace_logger<F>(s: F)
-    where
-        F: io::Write + Send + 'static,
-    {
-        LOGGER
-            .set(Logger::new(s))
-            .expect("Replacing the logger failed")
-    }
-
-    fn unwrap(&self) -> Result<sync::MutexGuard<'_, dyn io::Write + Send + 'static>, io::Error> {
-        self.inner
-            .lock()
-            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Mutex poisoned!"))
-    }
-
-    pub fn init<F>(s: F)
-    where
-        F: io::Write + Send + 'static,
-    {
-        log::set_max_level(log::LevelFilter::Trace);
-        log::set_logger(LOGGER.get_or_init(|| Logger::new(s)))
-            .expect("Cannot initialize the logger");
-    }
-
-    pub fn init_buffered<F>(s: F)
-    where
-        F: io::Write + Send + 'static,
-    {
-        log::set_max_level(log::LevelFilter::Trace);
-        log::set_logger(LOGGER.get_or_init(|| Logger::new_buffered(s)))
-            .expect("Cannot initialize the logger");
-    }
-}
-
-impl io::Write for Logger {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.unwrap().map(|mut lock| lock.write(buf))?
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.unwrap().map(|mut lock| lock.flush())?
     }
 }
 
@@ -91,17 +59,14 @@ impl Log for Logger {
     }
 
     fn flush(&self) {
-        match self.unwrap() {
-            Ok(mut lock) => {
-                let _ = lock.flush();
-            }
-            Err(_e) => self.inner.clear_poison(),
+        let mut inner = self.inner();
+        if let Inner::Other(inner) = &mut *inner {
+            inner.flush().expect("Cannot flush the logger");
         }
     }
 
     fn log(&self, record: &log::Record) {
         const RESET_COLOR: &'static str = "\x1b[0m";
-
         let (color, label) = match record.level() {
             Level::Info => ("\x1B[97m", "INF"),
             Level::Debug => ("\x1B[36m", "DBG"),
@@ -110,48 +75,100 @@ impl Log for Logger {
             Level::Trace => ("\x1B[97m", "TRC"),
         };
 
-        let thr = thread::current();
+        let current = std::thread::current();
+        let name = current.name().unwrap_or("unknown");
 
-        let thread = match thr.name() {
-            Some(name) => {
-                let mut s = name.to_owned();
-                s.truncate(5);
-                s
-            }
-            None => {
-                let id = thr.id().as_u64();
-                format!("id {id}")
-            }
-        };
+        let mut inner = self.inner();
+        let inner_ref = &mut *inner;
 
-        let result: Result<(), io::Error> = try {
-            let mut lock = self.unwrap()?;
-            writeln!(
-                lock,
-                "{}{color} {thread} [{label}]{RESET_COLOR} {}",
+        match inner_ref {
+            Inner::Stdout => println!(
+                "{}{color} {name} [{label}]{RESET_COLOR} {}",
                 Utc::now().format("%H:%M:%S"),
                 record.args()
-            )?;
-        };
+            ),
+            Inner::Other(mutex) => {
+                let err = writeln!(
+                    mutex,
+                    "{} {name} [{label}] {}",
+                    Utc::now().format("%H:%M:%S"),
+                    record.args()
+                );
 
-        result.expect("Cannot write to the logger")
+                err.expect("Cannot write to lock")
+            }
+            Inner::Stderr => eprintln!(
+                "{}{color} {name} [{label}] {RESET_COLOR} {}",
+                Utc::now().format("%H:%M:%S"),
+                record.args()
+            ),
+        };
     }
 }
 
-static LOGGER: sync::OnceLock<Logger> = sync::OnceLock::new();
+// impl Log for Logger {
+//     fn enabled(&self, _metadata: &log::Metadata) -> bool {
+//         true
+//     }
 
-#[inline]
-pub fn init<F>(f: F)
-where
-    F: io::Write + Send + 'static,
-{
-    Logger::init(f);
+//     fn flush(&self) {
+//         match self.unwrap() {
+//             Ok(mut lock) => {
+//                 let _ = lock.flush();
+//             }
+//             Err(_e) => self.inner.clear_poison(),
+//         }
+//     }
+
+//     fn log(&self, record: &log::Record) {
+//         const RESET_COLOR: &'static str = "\x1b[0m";
+
+//         let (color, label) = match record.level() {
+//             Level::Info => ("\x1B[97m", "INF"),
+//             Level::Debug => ("\x1B[36m", "DBG"),
+//             Level::Error => ("\x1B[31m", "ERR"),
+//             Level::Warn => ("\x1B[33m", "WRN"),
+//             Level::Trace => ("\x1B[97m", "TRC"),
+//         };
+
+//         let thr = thread::current();
+
+//         let thread = match thr.name() {
+//             Some(name) => {
+//                 let mut s = name.to_owned();
+//                 s.truncate(5);
+//                 s
+//             }
+//             None => {
+//                 let id = thr.id().as_u64();
+//                 format!("id {id}")
+//             }
+//         };
+
+//         let result: Result<(), io::Error> = try {
+//             let mut lock = self.unwrap()?;
+//             writeln!(
+//                 lock,
+//                 "{}{color} {thread} [{label}]{RESET_COLOR} {}",
+//                 Utc::now().format("%H:%M:%S"),
+//                 record.args()
+//             )?;
+//         };
+
+//         result.expect("Cannot write to the logger")
+//     }
+// }
+
+static LOGGER: LazyLock<Logger> = LazyLock::new(|| Logger::stdout());
+
+pub fn init_stdout() {
+    log::set_logger(&*LOGGER).expect("Cannot initialize the logger!");
 }
 
-#[inline]
-pub fn init_buffered<F>(f: F)
+pub fn swap<B>(buffer: B)
 where
-    F: io::Write + Send + 'static,
+    B: io::Write + Send + 'static,
 {
-    Logger::init_buffered(f);
+    let mut inner = LOGGER.inner();
+    *inner = Inner::Other(Box::new(buffer));
 }
